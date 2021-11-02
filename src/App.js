ae8d0017f4a39de82a385d4c4dd05ff2981ba2bb
ybucket_user <luka.lukicx@gmail.com>
2021-11-02 23:34:07 +0100

    app js change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>
-          Ma ide gas samo.
+          IDEMOOOOOOOOOOOOOOOOOO!!!
         </h2>
 
         <a
