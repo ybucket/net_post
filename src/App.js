b3b2c68130a8eb2e4a350a9473baf4dd4d71cf17
ybucket_user <luka.lukicx@gmail.com>
2021-11-02 23:12:59 +0100

    test msg commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Ma ide gas samo.
+        </h2>
+
+        <a
+          className="App-link"
+          href="https://tenor.com/view/zorica-markovic-turbofolk-farma-seme-gif-8365079"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          E jbg sad // <i>safe to click</i>
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
